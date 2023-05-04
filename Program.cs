33ff1a3b1fb27835f93d87af24b6e8c373e7ab3e
rikimaza <riki_30797@hotmail.es>
2023-05-04 20:08:37 +0200

    Creación de variables para almacenar la suma

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// initialize variables - graded assignments 
-int currentAssignments = 5;
+﻿int currentAssignments = 5;
 
 int sophia1 = 93;
 int sophia2 = 87;
@@ -24,3 +23,13 @@ int jeong2 = 92;
 int jeong3 = 98;
 int jeong4 = 100;
 int jeong5 = 97;
+
+int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
+int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
+int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
+
+Console.WriteLine("Sophia: " + sophiaSum);
+Console.WriteLine("Nicolas: " + nicolasSum);
+Console.WriteLine("Zahirah: " + zahirahSum);
+Console.WriteLine("Jeong: " + jeongSum);
