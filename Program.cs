efba9c9dd73153ed922629bb659118cf2de7cd6d
rikimaza <riki_30797@hotmail.es>
2023-05-06 13:19:04 +0200

    Segundo Ejercicio: Mostrar todas las características de las mascotas con datos asignados

--- Program.cs
+++ Program.cs
@@ -112,7 +112,11 @@ do
             {
                 if (ourAnimals[i, 0] != "ID #: ")
                 {
-                    Console.WriteLine(ourAnimals[i, 0]);
+                    Console.WriteLine();
+                    for (int j = 0; j < 6; j++)
+                    {
+                        Console.WriteLine(ourAnimals[i, j]);
+                    }
                 }
             }
             Console.WriteLine("Press the Enter key to continue.");
