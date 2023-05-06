b588400e210d7353fdac3c93523de6401a48019f
rikimaza <riki_30797@hotmail.es>
2023-05-06 13:52:59 +0200

    Cuarto Ejercicio: Creación de un bucle para leer y validar la edad de la mascota

--- Program.cs
+++ Program.cs
@@ -160,10 +160,25 @@ do
 
                     }
                 } while (validEntry == false);
-                
+
                 // build the animal the ID number - for example C1, C2, D3 (for Cat 1, Cat 2, Dog 3)
                 animalID = animalSpecies.Substring(0, 1) + (petCount + 1).ToString();
+                // get the pet's age. can be ? at initial entry. 
+                do
+                {
+                    int petAge;
+                    Console.WriteLine("Enter the pet's age or enter ? if unknown");
+                    readResult = Console.ReadLine();
+                    if (readResult != null)
+                    {
+                        animalAge = readResult.ToLower();
+                        if (animalAge != "?")
+                        {
+                            validEntry = int.TryParse(animalAge, out petAge);
+                        }
 
+                    }
+                } while (validEntry == false);
                 // increment petCount (the array is zero-based, so we increment the counter after adding to the array)
                 petCount = petCount + 1;
 
