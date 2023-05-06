aee3a97c13ab04bbcbd22bf8268cf672560d1bae
rikimaza <riki_30797@hotmail.es>
2023-05-06 13:46:56 +0200

    Cuarto Ejercicio: Creación de un bucle para leer y validar las especies de mascotas

--- Program.cs
+++ Program.cs
@@ -140,6 +140,26 @@ do
             }
             while (anotherPet == "y" && petCount < maxPets)
             {
+                bool validEntry = false;
+                // get species (cat or dog) - string animalSpecies is a required field 
+                do
+                {
+                    Console.WriteLine("\n\rEnter 'dog' or 'cat' to begin a new entry");
+                    readResult = Console.ReadLine();
+                    if (readResult != null)
+                    {
+                        animalSpecies = readResult.ToLower();
+                        if (animalSpecies != "dog" && animalSpecies != "cat")
+                        {
+                            validEntry = false;
+                        }
+                        else
+                        {
+                            validEntry = true;
+                        }
+
+                    }
+                } while (validEntry == false);
                 // increment petCount (the array is zero-based, so we increment the counter after adding to the array)
                 petCount = petCount + 1;
 
